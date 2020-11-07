cf27f9052139327639c3ebfd8a67c602fcbfb14f
ZIemniakoss <rawa.przemek@gmail.com>
2020-11-07 19:15:09 +0100

    Modyfikacja IProjectsService
    więcej enumów -> lepsze wiadomości o błędach i brak konieczności obsługi zbędnych błędów

--- server/editor-server/src/services/projects.rs
+++ server/editor-server/src/services/projects.rs
@@ -1,45 +1,63 @@
 use crate::repositories::users::User;
 use crate::models::Project;
+use crate::repositories::projects::IProjectsRepository;
+use std::fs::read_to_string;
 
 pub fn new(user: User) {}
 
 pub trait IProjectsService {
-	fn create(&self, project: Project) -> Result<Project, ProjectServiceError>;
-	fn update(&self, project: Project) -> Result<Project, ProjectServiceError>;
-	fn delete(&self, project: Project) -> Result<(), ProjectServiceError>;
-	fn grant_access(&self, project: Project, user: USer) -> Result<(), ProjectServiceError>;
-	fn revoke_access(&self, project: Project, user: USer) -> Result<(), ProjectServiceError>;
+	fn create(&self, name: String, description: String) -> Result<Project, SaveError>;
+	fn update(&self, project: Project) -> Result<Project, SaveError>;
+	fn delete(&self, project: Project) -> Result<(), DeleteError>;
+	fn grant_access(&self, project: &Project, user: &USer) -> Result<(), AccessGrantingError>;
+	fn revoke_access(&self, project: &Project, user: &USer) -> Result<(), AccessRevokingError>;
 }
 
-pub enum ProjectServiceError {
+pub enum DeleteError {
+	AccessDenied
+}
+
+pub enum AccessGrantingError {
+	NotOwner
+}
+
+pub enum AccessRevokingError {
+	IsOwner
+}
+
+pub enum SaveError {
+	InvalidName,
 	ProjectDoesNotExists,
-	NoAccess
+	AccessException,
 }
 
-
-struct ProjectsService{
-	user:User,
-
+struct ProjectsService {
+	user: User,
+	projects_repository: Box<dyn IProjectsRepository>,
 }
 
-impl IProjectsService for ProjectsService{
-	fn create(&self, project: Project) -> Result<Project, ProjectServiceError> {
+impl IProjectsService for ProjectsService {
+	fn create(&self, project: Project) -> Result<Project, SaveError> {
+		match self.projects_repository.create(project)){}
+	}
+
+	fn update(&self, project: Project) -> Result<Project, SaveError> {
+		if project.owner != self.user {
+			return Err(SaveError::AccessException);
+		}
 		unimplemented!()
 	}
 
-	fn update(&self, project: Project) -> Result<Project, ProjectServiceError> {
+	fn delete(&self, project: Project) -> Result<(), DeleteError> {
 		unimplemented!()
 	}
 
-	fn delete(&self, project: Project) -> Result<(), ProjectServiceError> {
+	fn grant_access(&self, project: &Project, user: &User) -> Result<(), AccessGrantingError> {
 		unimplemented!()
 	}
 
-	fn grant_access(&self, project: Project, user: _) -> Result<(), ProjectServiceError> {
-		unimplemented!()
-	}
-
-	fn revoke_access(&self, project: Project, user: _) -> Result<(), ProjectServiceError> {
+	fn revoke_access(&self, project: &Project, user: &User) -> Result<(), AccessRevokingError> {
 		unimplemented!()
 	}
 }
+
